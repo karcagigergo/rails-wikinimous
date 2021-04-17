25ad07a81c787a80ae26477ad60fa5738e9c6274
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-17 18:32:28 -0300

    Create done from CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,19 @@
 class ArticlesController < ApplicationController
+    before_action :find_article, only: [:show, :edit, :update, :destroy]
+    
+    def home; end
+
+    def index
+        @articles = Article.all
+    end
+
+    def show; end
+
+    
+
+    private
+
+    def find_article
+        @article = Article.find(params[:id])
+    end
 end
